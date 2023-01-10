dc67b742442e8948157e71211a032bebf52734aa
Kassim <kassimbouzoubaa05@gmail.com>
2023-01-10 18:29:27 +0100

    env

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://kassimbouzoubaa:SKkBjkQcnIcLZDsb@cluster0.8nmkskx.mongodb.net/weatherapp';
+const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
